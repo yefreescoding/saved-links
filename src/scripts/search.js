26431fd4093d37416ad75fcf1624bb699a0b7851
Yefree Valdez <yefree13santos@outlook.com>
2024-03-29 02:22:18 +0100

    fix: backdrop filter safari

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,10 +1,5 @@
 import dataLinks from "../data/mock-links.json";
-import {
-  generateLinkHTML,
-  shortedUrl,
-  displayLinks,
-  addLinkToDOM,
-} from "./function";
+import { generateLinkHTML } from "./function";
 
 const dialog = document.querySelector(".search_dialog");
 const showButton = document.querySelector("#search-btn");
@@ -25,35 +20,23 @@ window.addEventListener("keydown", (event) => {
     dialog.showModal();
     document.body.style.overflow = "hidden";
   }
-});
 
-window.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     document.body.style.overflow = "";
   }
 });
 
-// Sample array of objects (replace this with your actual data)
-const data = [
-  { id: 1, title: "Example 1", description: "Description 1" },
-  { id: 2, title: "Example 2", description: "Description 2" },
-  // Add more objects as needed
-];
-
 // Function to perform search
 function search(query, arr) {
-  // Clear previous search results
   const searchResults = document.getElementById("searchResults");
   searchResults.innerHTML = "";
 
-  // Filter the data based on the search query
   const filteredData = arr.filter(
     (item) =>
       item.title.toLowerCase().includes(query.toLowerCase()) ||
       item.link.toLowerCase().includes(query.toLowerCase())
   );
 
-  // Display search results
   if (filteredData.length > 0) {
     filteredData.forEach((item) => {
       const li = document.createElement("li");
@@ -62,12 +45,11 @@ function search(query, arr) {
     });
   } else {
     const li = document.createElement("li");
-    li.innerHTML = `<li class="main__link">No matches</li>`;
+    li.innerHTML = `<li class="main__link"><strong>No matches</strong></li>`;
     searchResults.appendChild(li);
   }
 }
 
-// Event listener for changes in the search input
 const searchInput = document.getElementById("search-input");
 const form = document.getElementById("search-form");
 form.addEventListener("submit", (e) => {
